f40779005b8fe9176b94d6c4dad48161263a41bd
quetto1 <richterkrzysztof89@gmail.com>
2023-12-28 19:59:08 +0100

    Routing Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
-import Header from "./components/Header"
+import Header from "./components/Header";
+import About from "./components/pages/about/About";
+import Music from "./components/pages/music/Music";
+import Events from "./components/pages/events/events";
+import Kupsko from "./components/pages/page4Chujwie/Kupsko";
+
+import { Route } from "react-router-dom";
+import { Routes } from "react-router-dom";
+
 function App() {
 
   return (
     <div>
       <Header></Header>
-      
+      <main>
+        <Routes>
+          <Route  path="/" element={<About></About>}></Route>
+          {/* <Route  path="/quetto1/VanillaWebsite" element={<About></About>}></Route> */}
+          {/* Extra linijka na homepage po zahostowaniu */}
+          <Route  path="/music" element={<Music></Music>}></Route>
+          <Route  path="/events" element={<Events></Events>}></Route>
+          <Route  path="/kupsko" element={<Kupsko></Kupsko>}></Route>
+        </Routes>
+      </main>
       <h1>Vite + React</h1>
    
       <p className="read-the-docs">
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,15 +2,17 @@ import "./Header.css"
 import "./GlitchSupport.css"
 import WobblyBackground from "../assets/headerBackgrounds/WobblyBackground.gif"
 
+import { NavHashLink as Link } from 'react-router-hash-link';
+
 function Header() {
     return (
         <div className="header-wrapper">
             <img className="vanilla-wobbly-img" src={WobblyBackground} alt="" />
             <div className="links-wrapper">
-                <div className="glitch-support">About</div>
-                <div className="glitch-support">Music</div>
-                <div className="glitch-support">Events</div>
-                <div className="glitch-support">Kupsko</div>
+                <div><Link to="/" className="glitch-support">About</Link></div>
+                <div><Link to="/music" className="glitch-support">Music</Link></div>
+                <div><Link to="/events" className="glitch-support">Events</Link></div>
+                <div><Link to="/kupsko" className="glitch-support">Kupsko</Link></div>
             </div>
         </div>
     );
