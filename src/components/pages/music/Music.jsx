8da9bd759c9904e8382672d094536faca7fd55da
quetto1 <richterkrzysztof89@gmail.com>
2024-01-13 14:23:21 +0100

    t

--- src/components/pages/music/Music.jsx
+++ src/components/pages/music/Music.jsx
@@ -8,21 +8,21 @@ import WobblyBackground from "../../../assets/headerBackgrounds/BackgroundsTest1
 
 
 function Music() {
-    return(
+    return (
         <div>
-        <Header img={WobblyBackground}></Header>
-        <PageHeader title="Music"></PageHeader>
-        <HeaderV1 title="DISCOVER DJ MIXES"></HeaderV1>
-        <YoutubeVideoComponent link="https://www.youtube.com/embed/HYTQc7sXvnk?start=1" description="VANILLA_SUMMER WAVE DJ _SET"></YoutubeVideoComponent>
-        <HeaderV1 title="LIVE VANILLA VILLAIN GIGS"></HeaderV1>
-        <MixCloudPlayer url="https://www.mixcloud.com/thevanillavillain/summer-drive-mix/"></MixCloudPlayer>
-        <MixCloudPlayer url="https://www.mixcloud.com/thevanillavillain/zooluft-exp4-vv-mix/"></MixCloudPlayer>
-        <MixCloudPlayer url="https://www.mixcloud.com/thevanillavillain/hip-hop-cut/"></MixCloudPlayer>
-        <MixCloudPlayer url="https://www.mixcloud.com/thevanillavillain/techno-cut/"></MixCloudPlayer>
-        <MixCloudPlayer url="https://www.mixcloud.com/thevanillavillain/disco-and-house/"></MixCloudPlayer>
-        <MixCloudPlayer url="https://www.mixcloud.com/thevanillavillain/part1/"></MixCloudPlayer>
-        <MixCloudPlayer url="https://www.mixcloud.com/thevanillavillain/part2/"></MixCloudPlayer>
-        <MixCloudPlayer url="https://www.mixcloud.com/thevanillavillain/p3/"></MixCloudPlayer>
+            <Header img={WobblyBackground}></Header>
+            <PageHeader title="Music"></PageHeader>
+            <HeaderV1 title="DISCOVER DJ MIXES"></HeaderV1>
+            <YoutubeVideoComponent link="https://www.youtube.com/embed/HYTQc7sXvnk?start=1" description="VANILLA_SUMMER WAVE DJ _SET"></YoutubeVideoComponent>
+            <HeaderV1 title="LIVE VANILLA VILLAIN GIGS"></HeaderV1>
+            <MixCloudPlayer url="https://www.mixcloud.com/thevanillavillain/summer-drive-mix/"></MixCloudPlayer>
+            <MixCloudPlayer url="https://www.mixcloud.com/thevanillavillain/zooluft-exp4-vv-mix/"></MixCloudPlayer>
+            <MixCloudPlayer url="https://www.mixcloud.com/thevanillavillain/hip-hop-cut/"></MixCloudPlayer>
+            <MixCloudPlayer url="https://www.mixcloud.com/thevanillavillain/techno-cut/"></MixCloudPlayer>
+            <MixCloudPlayer url="https://www.mixcloud.com/thevanillavillain/disco-and-house/"></MixCloudPlayer>
+            <MixCloudPlayer url="https://www.mixcloud.com/thevanillavillain/part1/"></MixCloudPlayer>
+            <MixCloudPlayer url="https://www.mixcloud.com/thevanillavillain/part2/"></MixCloudPlayer>
+            <MixCloudPlayer url="https://www.mixcloud.com/thevanillavillain/p3/"></MixCloudPlayer>
         </div>
     )
 }
