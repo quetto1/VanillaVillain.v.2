2c9ad6b40c6b0706a5dbc1eaf927142508c5c1fe
quetto1 <richterkrzysztof89@gmail.com>
2024-01-16 21:03:06 +0100

    Setup for Events Page and Photos

--- src/components/pages/events/Events.jsx
+++ src/components/pages/events/Events.jsx
@@ -2,13 +2,14 @@ import Header from "../../Header";
 import PageHeader from "../../pageHeader/PageHeader";
 
 import WobblyBackground from "../../../assets/headerBackgrounds/BackgroundsTest1/wibblyinvisibleperson.gif"
+import HeaderV1 from "../../smallComponents/headerV1/HeaderV1";
 
 function Events() {
     return(
         <div>
         <Header img={WobblyBackground}></Header>
         <PageHeader title="Events"></PageHeader>
-
+        <HeaderV1 title="Latests & Upcoming Events"></HeaderV1>
         </div>
     )
 }
