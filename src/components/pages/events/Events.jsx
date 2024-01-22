92362908e567bfa925f42f5d81bf290310f7c11f
quetto1 <richterkrzysztof89@gmail.com>
2024-01-22 15:14:32 +0100

    Second poster update

--- src/components/pages/events/Events.jsx
+++ src/components/pages/events/Events.jsx
@@ -2,10 +2,12 @@ import Header from "../../Header";
 import PageHeader from "../../pageHeader/PageHeader";
 import HeaderV1 from "../../smallComponents/headerV1/HeaderV1";
 import BasicPosterVertical from "../../postersComponents/BasicPosterVertical";
+import BasicPosterHorizontal from "../../postersComponents/BasicPosterHorizontal";
 
 import WobblyBackground from "../../../assets/headerBackgrounds/BackgroundsTest1/wibblyinvisibleperson.gif"
 import YellowPoster from "../../../assets/posterPictures/MarchMadnePoster.jpg"
 import GreenPoster from "../../../assets/posterPictures/FirstPartPoster.png"
+import ZooluftHorizontalPoster from "../../../assets/posterPictures/zooluft expoPoster.png"
 
 function Events() {
     return(
@@ -17,6 +19,8 @@ function Events() {
         <BasicPosterVertical img={YellowPoster}></BasicPosterVertical>
         <HeaderV1 title="Techno & Rave (Aarhus, Denmark, 26/11/2022)"></HeaderV1>
         <BasicPosterVertical img={GreenPoster}></BasicPosterVertical>
+        <HeaderV1 title="Zooluft Opener (Aarhus, Denmark, 25/06/2022)"></HeaderV1>
+        <BasicPosterHorizontal img={ZooluftHorizontalPoster}></BasicPosterHorizontal>
         </div>
     )
 }
---
+++ src/components/postersComponents/BasicPosterHorizontal.jsx
@@ -0,0 +1,11 @@
+ import './BasicPosterHorizontal.css'
+
+ function BasicPosterHorizontal(props) {
+    return (
+    <div className="basic-horizontal-poster-wrapper-main">
+        <img src={props.img}></img>            
+    </div>
+    )
+ }
+
+ export default BasicPosterHorizontal
